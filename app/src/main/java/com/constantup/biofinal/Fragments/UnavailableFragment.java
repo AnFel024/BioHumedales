823557a36d2a6c34ce71d2b8b846ce02812245b0
Andrés Felipe Morales <anfelpe.0200@gmail.com>
2021-02-01 13:07:33 -0500

    Creacion de un fragmento informativo

---
+++ app/src/main/java/com/constantup/biofinal/Fragments/UnavailableFragment.java
@@ -0,0 +1,27 @@
+package com.constantup.biofinal.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.constantup.biofinal.R;
+
+public class UnavailableFragment extends Fragment {
+    public UnavailableFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_unavailable, container, false);
+        return rootView;
+    }
+}
